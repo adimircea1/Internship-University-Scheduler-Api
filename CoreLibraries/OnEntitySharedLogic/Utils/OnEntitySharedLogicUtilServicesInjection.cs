e6d5a6d405e1cf5e27a456a5bcc2b107653c03aa
adimi <mircea.cremene@dataart.com>
2024-10-24 21:05:15 +0300

    injection fix

--- CoreLibraries/OnEntitySharedLogic/Utils/OnEntitySharedLogicUtilServicesInjection.cs
+++ CoreLibraries/OnEntitySharedLogic/Utils/OnEntitySharedLogicUtilServicesInjection.cs
@@ -1,8 +1,9 @@
 using Microsoft.Extensions.DependencyInjection;
 using OnEntitySharedLogic.DatabaseGenericRepository;
+using OnEntitySharedLogic.DistributedCache;
 using OnEntitySharedLogic.Services.LambdaExpressionCreator;
 
-namespace OnEntitySharedLogic.GRPC.Utils.InjectorClasses;
+namespace OnEntitySharedLogic.Utils;
 
 public class OnEntitySharedLogicUtilServicesInjection
 {
@@ -10,5 +11,6 @@ public class OnEntitySharedLogicUtilServicesInjection
     {
         services.AddScoped(typeof(IDatabaseGenericRepository<>), typeof(DatabaseGenericRepository<>));
         services.AddScoped<IExpressionBuilder, ExpressionBuilder>();
+        services.AddScoped<IDistributedCacheService, DistributedCacheService>();
     }
 }
